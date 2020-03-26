9509f6d3107e6ae122ab3f32d7e21ae0e8b7cc5b
sthancock <steven.hancock@ed.ac.uk>
2020-03-26 11:51:53 +0000

    First R script completed.

--- task1/plotOneFunc.R
+++ task1/plotOneFunc.R
@@ -17,9 +17,16 @@ setwd("/geos/netdata/env_geog/week_11/task1/func_form")
 # select a filename
 filename="funcForm.line.txt"
 
-# read the data in to RAM
-dataset <- read.csv(filename)
+# read the data, a space delimited text file, in to RAM
+dataset <- read.table(filename)
+
+# check the summary
+summary(dataset)
 
 # plot a graph
+plot(dataset$V1,dataset$V2,xlab="Biomass (Mg/ha)", ylab = "Predictor", cex=1, pch=19, col="blue") ## need to add colour
 
+# save that graph to a .png file to be used in a report
+dev.copy(png,'myplot.png')
+dev.off()
 
