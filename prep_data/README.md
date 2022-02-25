489bfb6b9597fba8bdd084d40666116e3b8ddd7c
sthancock <steven.hancock@ed.ac.uk>
2022-02-25 11:24:15 +0000

    Week 1 prep scripts started.

---
+++ prep_data/README.md
@@ -0,0 +1,4 @@
+# Data preparation
+
+This folder contains scripts to generate data needed for the course.
+
---
+++ prep_data/week1/makeGround.py
@@ -0,0 +1,10 @@
+'''
+Script to generate ground data
+Produces field tree data for
+use in week 1's exercise
+'''
+
+
+
+import argparse
+
