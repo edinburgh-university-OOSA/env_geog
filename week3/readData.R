cb416cf968d922cfd4eb241066df392ae3a59ef7
sthancock <steven.hancock@ed.ac.uk>
2022-03-26 11:13:47 +0000

    Week 11, task 1.1 code added.

---
+++ week3/readData.R
@@ -0,0 +1,18 @@
+
+# Script to practice function fitting
+
+# read data
+filename <- '/geos/netdata/env_geog/week10/function/dataset.1.csv'
+d <- read.csv(filename)
+
+# print data
+print(d)
+
+# determine number of points
+length(d$AGBD)
+
+# plot the data
+plot(d$CHM,d$AGBD,xlab="CHM (m)",ylab="AGBD (Mg/ha)")
+
+
+
